be00baf88569bbaf463a3ee5248fbc545765e404
claudioed <claudioed.oliveira@gmail.com>
2021-03-11 07:29:19 -0300

    fix pom.xml and main class entrypoint

--- src/main/java/tech/claudioed/users/Application.java
+++ src/main/java/tech/claudioed/users/Application.java
@@ -2,18 +2,24 @@ package tech.claudioed.users;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.micrometer.MicrometerMetricsOptions;
 import io.vertx.micrometer.VertxPrometheusOptions;
 
 public class Application {
 
+  private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
   public static void main(String[] args ){
+    LOG.info("Starting users deployment...");
     Vertx vertx = Vertx.vertx(new VertxOptions().setMetricsOptions(
       new MicrometerMetricsOptions()
         .setPrometheusOptions(new VertxPrometheusOptions().setEnabled(true))
         .setEnabled(true)));
     vertx.deployVerticle(new MainVerticle());
     vertx.deployVerticle(new CreateUserInIdentityProvider());
+    LOG.info("Users deployment was started successfully");
   }
 
 }
